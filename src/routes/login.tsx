0324d0d59d219d2cf97ded954eff90e2179aa755
Roni Tuohino <tuohinoroni@gmail.com>
2023-11-11 21:49:23 +0200

    Add code input to login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,50 +1,58 @@
-import { BasicImage } from '../components/imageForLogin'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import { BasicImage } from "../components/imageForLogin";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import CodeInput from "react-code-input";
+import { useNavigate } from "react-router";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <BasicImage imageUrl="https://icons.iconarchive.com/icons/iconarchive/dogecoin-to-the-moon/512/Doge-icon.png" />
-          <Typography component="h1" variant="h4" sx={{ mt: 2 }}>
-            Welcome to Tuska
-          </Typography>
-          <Typography component="h1" variant="h6" sx={{ mt: 6 }}>
-            Please enter the code your doctor provided you with:
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="AuthCode"
-              label="Authentification Code"
-              name="AuthCode"
-              autoFocus
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Continue
-            </Button>
-          </Box>
-        </Box>
-      </Container>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 2,
+      }}
+    >
+      <BasicImage imageUrl="https://icons.iconarchive.com/icons/iconarchive/dogecoin-to-the-moon/512/Doge-icon.png" />
+      <Typography
+        component="h1"
+        variant="h4"
+        textAlign="center"
+        sx={{ fontWeight: "600" }}
+      >
+        Welcome to Tuska
+      </Typography>
+      <Typography component="h1" variant="body1" textAlign="center">
+        Please, enter the code your doctor provided
+      </Typography>
+      <CodeInput
+        name="code"
+        type="text"
+        fields={6}
+        inputMode="full-width-latin"
+        forceUppercase
+        autoFocus
+        disabled={loading}
+        onChange={(value) => {
+          if (value.length === 6) {
+            setLoading(true);
+            setTimeout(() => {
+              navigate("/select");
+            }, 1000);
+          }
+        }}
+      />
+    </Container>
   );
 }
