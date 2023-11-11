f495cbf5fa030d4401efe31e229442f333f19ce9
Roni Tuohino <tuohinoroni@gmail.com>
2023-11-11 15:06:57 +0200

    Add vite base project

--- backend/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'
-import config from './utils/config'
-import logger from './utils/logger'
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
-
