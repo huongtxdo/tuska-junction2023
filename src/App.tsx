ed73557b86953d000dae6fb5fa2f71a11098183c
Roni Tuohino <tuohinoroni@gmail.com>
2023-11-11 16:26:20 +0200

    Add MUI and react-router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>test github actions deployment</div>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+import Root from "./routes/root.tsx";
+import Test from "./routes/test.tsx";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+    },
+    {
+      path: "/test",
+      element: <Test />,
+    },
+  ],
+  {
+    basename: "/tuska-junction2023/",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
